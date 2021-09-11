2ccc955caefeda1b468459bb66cc4ec9049472bf
sergekuv <sergekuv@yandex.ru>
2021-09-11 15:32:07 +0300

    Добавлена задача 5.2.17 и 5.2.18 и задачи модуля 5.3

--- Part1_Mod5/Program.cs
+++ Part1_Mod5/Program.cs
@@ -10,72 +10,13 @@ namespace Part1_Mod5
             //Task_5_1_5();
             //Task_5_1_6(); //она же 5.2.8, она же 5.2.14
             //Task_5_2_2(); //она же 5.2.3
-
+            //Task_5_2_17();
+            //Task_5_2_18();
+            //Task_5_3_1();
+            //Task_5_3_8();
+            Task_5_3_13();
 
             Console.WriteLine("\nEnd");
-        }
-
-        private static void Task_5_2_2()         //Вводим и выводим данные пользователя, включая цвета
-        {
-            Console.WriteLine("\n-- 5.2.2 --");
-            (string Name, int Age, string[] favColors) user = EnterUserFromConsole();
-            PrintUser(user);
-            Console.WriteLine("\n-- end of 5.2.2 --");
-        }
-
-        private static void PrintUser((string Name, int Age, string[] favColors) user)
-        {
-            Console.WriteLine("Информация о пользвоателе: ");
-            Console.Write($"Имя: {user.Name}; Вoзраст: {user.Age}; Любимые цвета: ");
-            PrintColors(user.favColors);
-        }
-
-        private static void  PrintColors(string[] favColors)
-        {
-            ConsoleColor currentForeground = Console.ForegroundColor;
-            ConsoleColor currentBackground = Console.BackgroundColor;
-            foreach (string item in favColors)
-            {
-                Console.ForegroundColor = ConsoleColor.Black;
-                switch (item)
-                {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        break;
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        break;
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        break;
-                    default :
-                        Console.BackgroundColor = ConsoleColor.Gray;
-                        Console.Write("(используем серый вместо) ");
-                        break;
-                }
-                Console.Write(" " + item + " ");
-            }
-            Console.ForegroundColor = currentForeground;
-            Console.BackgroundColor = currentBackground;
-            Console.WriteLine();
-        }
-
-        private static (string Name, int Age, string[] favColors) EnterUserFromConsole()  
-        {
-            //вводим информацию о пользователи с консоли
-            const int colorsLen = 3; //длина массива цветов
-            Console.Write("Введите имя: ");
-            string name = Console.ReadLine();
-            Console.Write("Введите возраст (целое число цифрами): ");
-            int age;
-            bool ageIsCorrect = Int32.TryParse(Console.ReadLine(), out age);
-            string[] colors = new string[colorsLen];
-            for (int i = 0; i < colorsLen; i++)
-            {
-                Console.Write("{2}, введите название цвета на английском ({0} из {1}): ", i+1, colorsLen, name); //исправлено в соотв. с требованием 5.2.3
-                colors[i] = Console.ReadLine().ToLower();
-            }
-            return (name, age, colors);
         }
 
         static void Task_5_1_1() //Задание 5.1.1
@@ -122,18 +63,133 @@ namespace Part1_Mod5
             Console.WriteLine("\n-- end of 5.1.5 --\n");
         }
 
-
-        private static void Task_5_1_6() // - она же 5.2.8  после ввода массива с клавиатуры необходимо отсортировать массив и вывести его на экран
+        private static void Task_5_1_6() // - она же 5.2.8, она же 5.2.15  после ввода массива с клавиатуры необходимо отсортировать массив и вывести его на экран
         {
             const int arrLen = 3;
             Console.WriteLine("\n-- 5.1.6 --");
-            int[] arr = GetArrayFromConsole(arrLen); //вводим массив
-            PrintArrayToConsole(ref arr);                   //выводим массив
+            int[] arr = GetArrayFromConsole(); //вводим массив
+            ShowArray(ref arr);                   //выводим массив
             SortArray(arr);                          //сортируем массив
-            PrintArrayToConsole(ref arr);                   //выводим массив
+            ShowArray(ref arr);                   //выводим массив
             Console.WriteLine("\n-- end of 5.1.6 --");
 
         }
+
+        private static void Task_5_2_2() //Вводим и выводим данные пользователя, включая цвета
+        {
+            Console.WriteLine("\n-- 5.2.2 --");
+            (string Name, int Age, string[] favColors) user = EnterUserFromConsole();
+            PrintUser(user);
+            Console.WriteLine("\n-- end of 5.2.2 --");
+        }
+
+        private static void Task_5_2_17() // Создайте метод ShowArray с параметрами: массив чисел, признак сортировки логического типа,
+                                          // необязательный параметр, по умолчанию ЛОЖЬ. Метод должен выводить массив на экран.
+                                          // Если значение признака сортировки равно ИСТИНА, то предварительно массив нужно отсортировать.
+        {
+            const int arrLen = 3;
+            Console.WriteLine("\n-- 5.2.17 --");
+            int[] arr = GetArrayFromConsole(); //вводим массив
+            Console.WriteLine("Вызов ShowArray без сортировки");
+            ShowArray(ref arr);                   //выводим массив
+            Console.WriteLine("Вызов ShowArray с сортировкой");
+            ShowArray(ref arr, sort: true);       //выводим массив
+            Console.WriteLine("\n-- end of 5.2.18 --");
+
+        }
+
+        private static void Task_5_2_18() // Создайте метод ShowArray с параметрами: массив чисел, признак сортировки логического типа,
+                                          // необязательный параметр, по умолчанию ЛОЖЬ. Метод должен выводить массив на экран.
+                                          // Если значение признака сортировки равно ИСТИНА, то предварительно массив нужно отсортировать.
+        {
+            Console.WriteLine("\n-- 5.2.18 --");
+            int[] arr = GetArrayFromConsole(arrLen: 10); //вводим массив
+            Console.WriteLine("Вызов ShowArray без сортировки");
+            ShowArray(ref arr);                   //выводим массив
+            Console.WriteLine("Вызов ShowArray с сортировкой");
+            ShowArray(ref arr, sort: true);       //выводим массив
+            Console.WriteLine("\n-- end of 5.2.18 --");
+
+        }
+        private static void Task_5_3_1() //Проверяем передачу по ссылке и по значению
+        {
+            Console.WriteLine("\n-- 5.3.1 --");
+            int[] arr = { 1, 8, 3, 2, 8 };
+            ShowArray(ref arr);
+            int[] arr2 = arr;
+            SortArray(arr2);
+            ShowArray(ref arr);
+            Console.WriteLine("\n-- 5.3.1 --");
+        }
+        private static void Task_5_3_5() // ВОкруг задания 5.3.5: проверяем работу "in"
+        {
+            var arr = new int[] { 1, 2, 3 };
+            Console.WriteLine(arr[0]);
+            BigDataOperation(arr, arr[0]);
+            Console.WriteLine(arr[0]);
+        }
+
+        private static void Task_5_3_8() // Необходимо передать по ссылке размерность массива в метод GetArrayFromConsole и изменить её на 6.
+        {
+            Console.WriteLine("\n-- 5.3.8 --");
+            int arrLen = 10;
+            Console.WriteLine($"ArrLen before GetArray...Ref: {arrLen}");
+            int[] arr = GetArrayFromConsoleRef(ref arrLen); //вводим массив
+            Console.WriteLine($"ArrLen after GetArray...Ref: {arrLen}");
+            Console.WriteLine("Вызов ShowArray без сортировки");
+            ShowArray(ref arr);                   //выводим массив
+            Console.WriteLine("\n-- end of 5.3.8 --");
+
+        }
+        private static void Task_5_3_13() //Используйте код метода SortArray. Сейчас этот метод сортирует массив по возрастанию значения.
+        //Создайте методы SortArrayDesc и SortArrayAsc — сортировка массива от большего к меньшему и сортировка массива от меньшего к большему.
+        //Метод SortArray модифицируйте так, чтобы он вызвал оба этих метода.Результаты методов SortArrayAsc и SortArrayDesc должны представлять собой массивы.
+        //Метод SortArray должен иметь один входной параметр array и два выходных: sorteddesc и sorted asc.
+        {
+            const int arrLen = 3;
+            Console.WriteLine("\n-- 5.3.13 --");
+            int[] arr = GetArrayFromConsole(); //вводим массив
+            SortArray_5_3_13(in arr, out int[] sortedAsc, out int[] sortedDesc);                       //сортируем массив
+            Console.WriteLine("Initial array");
+            ShowArray(ref arr);                   //выводим массив
+            Console.WriteLine("Sorted Asc");
+            ShowArray(ref sortedAsc);
+            Console.WriteLine("Sorted Desc");
+            ShowArray(ref sortedDesc);
+
+            Console.WriteLine("\n-- end of 5.3.13 --");
+        }
+
+        private static void SortArray_5_3_13(in int[] arr, out int[] sorterAsc, out int[] sortedDesc)
+        {
+            sorterAsc = SortArrayAsc(arr);
+            sortedDesc = SortArrayDesc(arr);
+        }
+
+        private static int[] SortArrayAsc(int[] arr)
+        {
+            int[] tempArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                tempArr[i] = arr[i];
+            }
+            Array.Sort(tempArr);
+            return tempArr;
+        }
+
+        private static int[] SortArrayDesc(int[] arr)
+        {
+            int[] tempArr = SortArrayAsc(arr);
+            Array.Reverse(tempArr);
+            return tempArr;
+        }
+
+        static void BigDataOperation(in int[] arr, in int item)
+        {
+            arr[0] = 4;
+            //item = 77; //Comp. Error
+        }
+
 
         private static void SortArray(int[] arr) 
         {
@@ -154,8 +210,12 @@ namespace Part1_Mod5
             }
         }
 
-        private static void PrintArrayToConsole(ref int[] arr)  //выводим элементы массива на консоль
+        private static void ShowArray(ref int[] arr, bool sort = false )  //выводим элементы массива на консоль
         {
+            if (sort)
+            {
+                SortArray(arr);
+            }
             Console.Write("Вот элементы массива: ");
             foreach (int i in arr)
             {
@@ -164,7 +224,7 @@ namespace Part1_Mod5
             Console.WriteLine();
         }
 
-        private static int[] GetArrayFromConsole(int arrLen)    //вводим элементы массива с консоли
+        private static int[] GetArrayFromConsole(int arrLen = 3)    //вводим элементы массива с консоли
         {
             int[] myArray = new int[arrLen];
             for(int i=0; i<arrLen; i++)
@@ -175,6 +235,17 @@ namespace Part1_Mod5
             return myArray;
         }
 
+        private static int[] GetArrayFromConsoleRef(ref int arrLen)    //вводим элементы массива с консоли
+        {
+            arrLen = 6;
+            int[] myArray = new int[arrLen];
+            for (int i = 0; i < arrLen; i++)
+            {
+                Console.Write($"Введите целое число - элемент массива номер {i + 1} из {arrLen}: ");
+                bool enteredInt = int.TryParse(Console.ReadLine(), out myArray[i]);
+            }
+            return myArray;
+        }
 
         static string GetColorFromConsole() => Console.ReadLine().ToLower();  //нужны ли такие "однооператорные" методы? Попросить пару примеров, когда они нужны
         private static void ShowColor(string color, string text = "Вы выбрали")  //Временно меняем цвет фона консоли на выбранный цвет
@@ -202,5 +273,62 @@ namespace Part1_Mod5
             Console.ForegroundColor = initForeground;
             Console.BackgroundColor = initBackground;
         }
+
+        private static void PrintUser((string Name, int Age, string[] favColors) user)
+        {
+            Console.WriteLine("Информация о пользвоателе: ");
+            Console.Write($"Имя: {user.Name}; Вoзраст: {user.Age}; Любимые цвета: ");
+            PrintColors(user.favColors);
+        }
+
+        private static void PrintColors(string[] favColors)
+        {
+            ConsoleColor currentForeground = Console.ForegroundColor;
+            ConsoleColor currentBackground = Console.BackgroundColor;
+            foreach (string item in favColors)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                switch (item)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.Write("(используем серый вместо) ");
+                        break;
+                }
+                Console.Write(" " + item + " ");
+            }
+            Console.ForegroundColor = currentForeground;
+            Console.BackgroundColor = currentBackground;
+            Console.WriteLine();
+        }
+
+        private static (string Name, int Age, string[] favColors) EnterUserFromConsole()
+        {
+            //вводим информацию о пользователи с консоли
+            const int colorsLen = 3; //длина массива цветов
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите возраст (целое число цифрами): ");
+            int age;
+            bool ageIsCorrect = Int32.TryParse(Console.ReadLine(), out age);
+            string[] colors = new string[colorsLen];
+            for (int i = 0; i < colorsLen; i++)
+            {
+                Console.Write("{2}, введите название цвета на английском ({0} из {1}): ", i + 1, colorsLen, name); //исправлено в соотв. с требованием 5.2.3
+                colors[i] = Console.ReadLine().ToLower();
+            }
+            return (name, age, colors);
+        }
+
+
     }
 }
